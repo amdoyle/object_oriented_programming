bb6217f42a5dc30e6f4da37371eec59450a2bf6c
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-01-15 18:34:03 -0500

    complete mars rover

--- mars_rover2.rb
+++ mars_rover2.rb
@@ -2,34 +2,35 @@ class Rover
   attr_accessor = :start_x
                   :start_y
                   :direction
-                  # :dir
 
   def initialize (start_x, start_y, direction)
     @start_x = start_x
     @start_y = start_y
     @direction = direction
-    # @dir = dir
   end
 
-  def read_instructions(dir)
+  def read_instructions(dir, max_x, max_y)
     dir.each do |letter|
-    case letter
-    when "R"
+      if letter == "R"
         turn_right
-      when "L"
+      elsif letter == "L"
         turn_left
-      when "M"
+      elsif letter == "M"
         move
       else
-        puts "you crazy!"
+        puts "Sorry, that's an invaild output. Try again."
     end
     end
-  puts "I am at #{@start_x} #{@start_y} #{@direction}"
+    if max_x < @start_x || max_y < @start_y
+      puts "Mars Rover DOWN!"
+    else
+      puts "I am at #{@start_x} #{@start_y} #{@direction}"
+    end
   end
 
   def turn_left
     case @direction
-      when "N"
+    when "N"
         @direction= "W"
       when "S"
         @direction= "E"
@@ -38,7 +39,7 @@ class Rover
       when "W"
          @direction = "S"
        else
-         puts "you crazy! how did this happen?"
+         puts "Sorry, that's an invaild output. Try again."
     end
   end
 #
@@ -53,7 +54,7 @@ class Rover
       when "W"
         @direction  = "N"
       else
-        puts "what are you doing?"
+        puts "Sorry, that's an invaild output. Try again."
     end
   end
 
@@ -68,7 +69,7 @@ class Rover
       when "W"
         @start_x = @start_x - 1
       else
-        puts "really?"
+      puts "Sorry, that's an invaild output. Try again."
       end
   end
 
@@ -78,18 +79,39 @@ end
 
 puts "How wide is the area you would like to explore (please input two numbers seperated by a space)?"
 max_x, max_y = gets.chomp.split(" ")
-max_x.to_i
-max_y.to_i
+max_x = max_x.to_i
+max_y = max_y.to_i
 
-puts "Where would you like your rover to start? Please input two coordinate and a direction (N, E, S, W):"
+# puts "Where would you like your rover to start? Please input two coordinate and a direction (N, E, S, W):"
+# start_x, start_y, direction = gets.chomp.split(" ")
+# start_x = start_x.to_i
+# start_y = start_y.to_i
+#
+#
+# puts "Input your rover's movements (use L for left, R for right, and M to move forward one unit):"
+# dir = gets.chomp.split("")
+
+
+puts "Where would you like Mars Rover ONE to start? Please input two coordinate and a direction (N, E, S, W):"
+start_x, start_y, direction = gets.chomp.split(" ")
+start_x = start_x.to_i
+start_y = start_y.to_i
+direction = direction.upcase
+
+
+puts "Input the movements for Mars Rover ONE (use L for left, R for right, and M to move forward one unit):"
+dir = gets.chomp.upcase.split("")
+
+rover1 = Rover.new(start_x, start_y, direction)
+rover1.read_instructions(dir, max_x, max_y)
+
+puts "Where would you like Mars Rover TWO to start? Please input two coordinate and a direction (N, E, S, W):"
 start_x, start_y, direction = gets.chomp.split(" ")
 start_x = start_x.to_i
 start_y = start_y.to_i
 
+puts "Input the movements for Mars Rover TWO (use L for left, R for right, and M to move forward one unit):"
+dir = gets.chomp.upcase.split("")
 
-puts "Input your rover's movements (use L for left, R for right, and M to move forward one unit):"
-dir = gets.chomp.split("")
-
-rover1 = Rover.new(start_x, start_y, direction)
-
-rover1.read_instructions(dir)
+rover2 = Rover.new(start_x, start_y, direction)
+rover2.read_instructions(dir, max_x, max_y)
